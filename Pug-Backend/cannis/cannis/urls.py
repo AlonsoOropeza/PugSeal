689c8d6036ea2738ffb0f1a16c3c1464e608f688
irvingpercam <a01703171@itesm.mx>
2020-10-02 21:25:19 -0500

    Upload: Category registration

--- Pug-Backend/cannis/cannis/urls.py
+++ Pug-Backend/cannis/cannis/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include 
+from pugsealapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^crearCategoria/', views.crearCategoria),
+
 ]
---
+++ Pug-Backend/cannis/pugsealapp/serializer.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from pugsealapp.models import Area, Categoria, Empleado, Hotel, Ubicacion, Proveedor
+
+class AreaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Area
+        fields = ('id_area','nombre')
+
+class CategoriaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Categoria
+        fields = ('id_categoria','nombre')
+
+class EmpleadoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Empleado
+        fields = ('id_empleado','nombre','a_paterno','a_materno','correo_electronico','telefono')
+
+class HotelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Hotel
+        fields = ('id_hotel','nombre')
+
+class UbicacionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ubicacion
+        fields = ('id_ubicacion','nombre')
+
+class ProveedorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Proveedor
+        fields = ('id_proveedor','nombre')

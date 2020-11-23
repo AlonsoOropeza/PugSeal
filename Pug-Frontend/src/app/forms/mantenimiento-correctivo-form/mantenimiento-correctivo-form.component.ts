a034dcf4a832784d1bda7a322795b609c65bc42f
Daniel Zavalza <a01365099@itesm.mx>
2020-11-23 14:07:34 -0600

    calcula semana

--- Pug-Frontend/src/app/forms/mantenimiento-correctivo-form/mantenimiento-correctivo-form.component.ts
+++ Pug-Frontend/src/app/forms/mantenimiento-correctivo-form/mantenimiento-correctivo-form.component.ts
@@ -4,7 +4,7 @@ import { NgForm } from '@angular/forms';
 import { Categoria, Usuario, MantenimientoCorrectivo, Proveedor , Hotel, Area} from 'app/models/models.model';
 import { EventEmitter } from '@angular/core';
 import { getWeek } from 'date-fns';
-import { DatePipe } from '@angular/common';
+import { DatePipe, formatDate } from '@angular/common';
 @Component({
   selector: 'app-mantenimiento-correctivo-form',
   templateUrl: './mantenimiento-correctivo-form.component.html',
@@ -26,22 +26,19 @@ export class MantenimientoCorrectivoFormComponent implements OnInit{
   @Output() cancelparent = new EventEmitter();
   public auditor: any;
   @Input() user: Usuario;
-  public week: any;
   public startDate: any;
   public yearStart: any;
 
+
   ngOnInit(): void{
     
-    console.log(this.startDate);
-    this.getWeek(this.startDate);
+   // console.log(this.startDate);
+   // this.getWeek(this.startDate);
   }
 
-  constructor(){
-    this.startDate = new Date().toISOString().substring(0, 10);
-    console.log(this.startDate);
-  }
 
   public continue(form: NgForm) {
+    //this.mantenimiento.semana = 
     this.continueparent.emit(form);
   }
 
@@ -49,20 +46,28 @@ export class MantenimientoCorrectivoFormComponent implements OnInit{
     this.cancelparent.emit();
   }
 
-  public getWeek(date: any){
-    //console.log(this.date);
+  updateWeek(value: number){
+    console.log(value);
+    this.mantenimiento.semana = value;
+  }
+
+  /*public getWeek(date: any){
+    console.log(this.mantenimiento.semana);
     date = new Date();
-    //console.log(date);
+    console.log(date);
     date = Date.UTC(date.getFullYear(), date.getMonth(), date.getDate());
     date = new Date();
-    //console.log(date);
+    console.log(date);
     date.setUTCDate(date.getUTCDate() + 4 - (date.getUTCDay() || 7));
 
     this.yearStart = new Date(Date.UTC(date.getUTCFullYear(), 0, 1));
 
-    this.week = Math.ceil(( ( (date - this.yearStart) / 86400000) + 1) / 7);
+    //let latest_date = this.datepipe.transform(new Date(), 'dd-MM-yyyy');
+    //this.mantenimiento.fecha_solicitud = new Date(Date.parse(Date()));;
+    console.log(this.mantenimiento.fecha_solicitud);
+  
+    this.mantenimiento.semana = Math.ceil(( ( (date - this.yearStart) / 86400000) + 1) / 7);
 
-
-  } 
+  } */
 
 }

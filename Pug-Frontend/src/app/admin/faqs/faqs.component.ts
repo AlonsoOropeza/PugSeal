ba3f8afaf53af604ff6ccc2d6bdcab493cb97506
Raymundo <A00570654@itesm.mx>
2020-12-06 18:05:48 -0600

    Integrar FAQs con mejoras a defectos

--- Pug-Frontend/src/app/admin/faqs/faqs.component.ts
+++ Pug-Frontend/src/app/admin/faqs/faqs.component.ts
@@ -31,7 +31,6 @@ export class FaqsComponent implements OnInit {
   }
 
   public async loadInfo() {
-
     this.spinner.hideSpinner();
   }
 }
--- Pug-Frontend/src/app/app.module.ts
+++ Pug-Frontend/src/app/app.module.ts
@@ -53,8 +53,7 @@ import { FinalizarMantCorFormComponent } from './forms/finalizar-mant-cor-form/f
 import { HotelesComponent } from './admin/hoteles/hoteles.component';
 import { HotelFormComponent } from './forms/hotel-form/hotel-form.component';
 import { CalificarMantCorFormComponent } from './forms/calificar-mant-cor-form/calificar-mant-cor-form.component';
-import { FaqsComponent } from './faqs/faqs.component';
-
+import { FaqsComponent } from './admin/faqs/faqs.component';
 
 @NgModule({
   imports: [
